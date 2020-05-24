fb501e04720dae62ef2d745ebfadf6cc4e33950e
Brian Durand <briandurand1990@gmail.com>
2020-05-24 14:21:46 -0300

    * Se coloca un grid para la seccion de proyectos. * Se cambian valores del grid para Guias. * Se arregla warning de consola.

--- components/Proyects/index.jsx
+++ components/Proyects/index.jsx
@@ -5,35 +5,36 @@ const Proyects = () => {
   return (
     <section className={style.Proyects}>
       <h3>Proyectos</h3>
-      <div className="flex">
+      <div className={style.Grid}>
         <Card
           title={"Proyecto 1"}
           href={"/[_path]"}
-          item={style.card__proyect}
+          item={style.item__1}
           link={true}
           path={"/lista de proyectos"}
         />
         <Card
           title={"Proyecto 2"}
           href={"/[_path]"}
-          item={style.card__proyect}
+          item={style.item__2}
           link={true}
           path={"/lista de proyectos"}
         />
         <Card
           title={"Proyecto 3"}
           href={"/[_path]"}
-          item={style.card__proyect}
+          item={style.item__3}
+          link={true}
+          path={"/lista de proyectos"}
+        />
+        <Card
+          title={"Ver Más"}
+          href={"/[_path]"}
+          item={style.item__4}
           link={true}
           path={"/lista de proyectos"}
         />
       </div>
-      <Card
-        title={"Ver Más"}
-        href={"/[_path]"}
-        link={true}
-        path={"/lista de proyectos"}
-      />
     </section>
   );
 };
