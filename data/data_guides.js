51292129fdc126d4b35762e714eaa0c1b4039d95
Brian Durand <briandurand1990@gmail.com>
2020-09-06 16:11:10 -0300

    [Update] Guides - Le agrego un par de guías que estoy armando.

--- data/data_guides.js
+++ data/data_guides.js
@@ -1,10 +1,10 @@
 const DATA_GUIDES = {
   newest: [
     {
-      title: "Python",
+      title: "Local/Session Storage",
       url:
-        "https://www.notion.so/tecknchips/Python-8ef09227c1f74704ba494e1e7da544bb",
-      type: "python",
+        "https://www.notion.so/tecknchips/Local-Session-Storage-a654dbea32fd4b2d951c60251185af9d",
+      type: "javascript",
     },
     {
       title: "Diagrama de Flujo de Datos",
@@ -21,29 +21,72 @@ const DATA_GUIDES = {
   guides: [
     {
       id: 1,
-      title: "CSS Grid",
-      url: "https://www.notion.so/CSS-Grid-88eb436451bb4277ac40a15893dcfc50",
-      type: "css",
+      title: "Javascript",
+      url:
+        "https://www.notion.so/tecknchips/Javascript-662276b99a2c4161ac11e0f63fce0a25",
+      type: "javascript",
     },
     {
       id: 2,
-      title: "Sanity",
-      url: "https://www.notion.so/Sanity-84b4f7f719eb485c9ea87ddf8f497872",
+      title: "Métodos",
+      url:
+        "https://www.notion.so/tecknchips/M-todos-JS-a6d8c404183448e08ad1f698b7e6cac7",
       type: "javascript",
     },
     {
       id: 3,
+      title: "Objetos Javascript",
+      url:
+        "https://www.notion.so/tecknchips/Objetos-08a9081337954a10bd4b8433672fbea8",
+      type: "javascript",
+    },
+    {
+      id: 4,
+      title: "CSS Grid",
+      url: "https://www.notion.so/CSS-Grid-88eb436451bb4277ac40a15893dcfc50",
+      type: "css",
+    },
+
+    {
+      id: 5,
       title: "Diagrama de Flujo de Datos",
       url:
         "https://www.notion.so/tecknchips/Diagrama-de-Flujo-de-Datos-634bd2b62c8a45dbaa38aee63382888a",
       type: "sistemas",
     },
     {
-      id: 3,
+      id: 6,
       title: "Python Básico",
       url:
         "https://www.notion.so/tecknchips/Python-8ef09227c1f74704ba494e1e7da544bb",
       type: "python",
+    },
+    {
+      id: 7,
+      title: "Diccionario de datos",
+      url:
+        "https://www.notion.so/tecknchips/Diccionario-de-Datos-7754687a1ad9410ea17609806fdc1387",
+      type: "sistemas",
+    },
+    {
+      id: 8,
+      title: "Local/Session Storage",
+      url:
+        "https://www.notion.so/tecknchips/Local-Session-Storage-a654dbea32fd4b2d951c60251185af9d",
+      type: "javascript",
+    },
+    {
+      id: 9,
+      title: "OOP",
+      url:
+        "https://www.notion.so/tecknchips/OOP-0decd109bc044b479a123160fcd79df1",
+      type: "sistemas",
+    },
+    {
+      id: 10,
+      title: "Sanity",
+      url: "https://www.notion.so/Sanity-84b4f7f719eb485c9ea87ddf8f497872",
+      type: "javascript",
     },
   ],
 };
